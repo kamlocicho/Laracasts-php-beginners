0d2aafacad1de7780cfd86f5ca0c32592261a61c
Kamil Orwat <orwat.kamil3@gmail.com>
2023-02-17 21:26:15 +0100

    PHP Partials

---
+++ views/index.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
+        <?php require('partials/nav.php'); ?>
+
+        <header class="bg-white shadow">
+            <div class="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
+                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+            </div>
+        </header>
+
+        <main>
+            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                <p>Hello welcome to the home page</p>
+            </div>
+        </main>
+    </div>
+
+</body>
+
+</html>
